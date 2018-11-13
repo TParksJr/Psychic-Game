331874ef52d6d10708517afa6c199459b20a9df7
Tyson <tys_p@yahoo.ca>
2018-11-13 10:37:04 -0500

    added additional mobile responsive elements

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,4 +1,4 @@
-//attempt #5
+//declaring initial values
 var wins = 0,
     losses = 0,
     remaining = 9,
@@ -9,7 +9,6 @@ var wins = 0,
 
 //generate a random letter for computer choice
 letter = alphabet[Math.round(Math.random() * alphabet.length)];
-console.log("the letter is " + letter);
 
 //set all the values for the scoreboard
 document.getElementById("wins").innerHTML = wins;
@@ -20,7 +19,6 @@ document.getElementById("guessed").innerHTML = guessed;
 //store key pressed in var userGuess
 document.onkeypress = function (event) {
     userGuess = event.key;
-    console.log("userGuess is " + userGuess);
 
     //check if the current letter has been guessed already
     for (i = 0; i < guessed.length; i++) {
@@ -32,11 +30,9 @@ document.onkeypress = function (event) {
     //if they guess correctly increase wins, reset and alert
     if (userGuess == letter){
         wins += 1;
-        console.log("wins is " + wins);
         remaining = 9;
         guessed = [];
         letter = alphabet[Math.round(Math.random() * alphabet.length)];
-        console.log("the letter is " + letter);
         document.getElementById("wins").innerHTML = wins;
         document.getElementById("losses").innerHTML = losses;
         document.getElementById("remaining").innerHTML = remaining;
@@ -47,11 +43,9 @@ document.onkeypress = function (event) {
     //if they guess incorrectly and are on their last life increase losses, reset and alert
     else if (remaining == 1) {
         losses += 1;
-        console.log("losses is " + losses);
         remaining = 9;
         guessed = [];
         letter = alphabet[Math.round(Math.random() * alphabet.length)];
-        console.log("the letter is " + letter);
         document.getElementById("wins").innerHTML = wins;
         document.getElementById("losses").innerHTML = losses;
         document.getElementById("remaining").innerHTML = remaining;
@@ -69,169 +63,3 @@ document.onkeypress = function (event) {
         document.getElementById("guessed").innerHTML = guessed;
     };
 };
-
-/*attempt #4
-    //Available choices
-    var letterChoices = ['a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-    //score
-    var wins = 0;
-    var losses = 0;
-    var guesses = 9;
-    var guessesLeft = 9;
-    var guessedLetters = [];
-    var letterToGuess = null;
-    
-    //computer randomly chooses a letter
-    
-    var computerGuess = letterChoices [Math.floor(Math.random()*letterChoices.length)];
-    
-    //guesses left function
-    
-    var updateGuessesLeft = function() {
-        document.querySelector('#remaining').innerHTML = "Guesses Left: " + guessesLeft;
-    };
-    
-    //letter to guess function
-    
-    var updateletterToGuess = function(){
-        this.letterToGuess = this.letterChoices[Math.floor(Math.random() * this.letterChoices.length)];
-    };
-    
-    var updateGuessesSoFar = function(){
-        document.querySelector('#guessed').innerHTML = "Your guesses so far: " + guessedLetters.join(', ');
-    };
-    
-    //reset
-    
-    var reset = function(){
-        totalGuesses = 9;
-        guessesLeft = 9;
-        guessedLetters = [];
-    
-        updateletterToGuess();
-        updateGuessesSoFar();
-        updateGuessesLeft();
-    
-    };
-    
-    updateGuessesLeft();
-    updateletterToGuess();
-    
-    //user input key
-    
-    document.onkeyup = function(event) {
-        guessesLeft--;
-        var userGuess;
-    
-        guessedLetters.push(userGuess);
-        updateGuessesLeft();
-        updateGuessesSoFar();
-    
-            if (guessesLeft > 0){
-                if (event.key == letterToGuess){
-                    wins++;
-                    document.querySelector('#wins').innerHTML = 'Wins: ' + wins;
-                    alert("How did you know!?!");
-                    reset();
-                }
-            } else if (guessesLeft == 0){
-                losses++;
-                document.querySelector('#losses').innerHTML = 'Losses: ' + losses;
-                alert("Sorry, you're not a psychic!");
-    
-                reset();
-            }
-    }
-
-/* declaring variables
-var wins = 0,
-    losses = 0,
-    remaining = 9,
-    guessed = [],
-    alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-    letter = "",
-    userGuess = ""
-
-//generate a random letter for computer choice
-letter = alphabet[Math.round(Math.random() * alphabet.length)];
-
-attempt #3
-var reset = remaining = 9, guessed = [];
-
-document.onkeyup = function(event) {
-    remaining--;
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-    guessed.push(userGuess);
-    reset;
-
-        if (remaining > 0){
-            if (userGuess == letter){
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                alert("Yes, you are psychic!");
-                reset();
-            }
-        }else if(guessesLeft == 0){
-            // Then we will loss and we'll update the html to display the loss 
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("Sorry, you're not psychic, maybe try again?");
-            // Then we'll call the reset. 
-            reset();
-        }
-};*/
-
-/*attempt #2
-document.onkeyup = function(event) {
-    userGuess = event.key;
-    if (userGuess == letter){
-        wins++;
-        remaining = 9;
-        guessed = [];
-        document.alert("You won!");
-    }
-    //add a loss if the user runs out of guesses
-    else if (remaining == 1) {
-        losses++;
-        remaining = 9;
-        guessed = [];
-        document.alert("You lost!");
-    }
-    else {
-    //add key pressed to the guessed array and subtract one remaining guess
-    guessed.push(userGuess);
-    remaining--;
-    }
-};*/
-/*
-//attempt #1
-//create click event and have it call userGuess function
-document.addEventListener("keypress", userGuess());
-console.log("userGuess");
-//declaring userGuess function
-function userGuess(event) {
-    //add a point if the player guesses correctly
-    if (event.key == letter){
-        wins++;
-        remaining = 9;
-        guessed = [];
-        document.alert("You won!");
-    }
-    //add a loss if the user runs out of guesses
-    else if (remaining == 1) {
-        losses++;
-        remaining = 9;
-        guessed = [];
-        document.alert("You lost!");
-    }
-    else {
-    //add key pressed to the guessed array and subtract one remaining guess
-    guessed.push(event.key);
-    remaining--;
-    }
-    console.log("userGuess")
-    console.log("event.key")
-}
-*/
